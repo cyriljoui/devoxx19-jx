71d625829009973c554d59bf88ecefa52ab7c6e9
cjoui <cyril.joui@clip2.pro>
2019-04-17 11:46:11 +0200

    [NEW] Test PR

--- src/main/java/com/github/jenkinsx/quickstarts/springboot/rest/prometheus/RestPrometheusApplication.java
+++ src/main/java/com/github/jenkinsx/quickstarts/springboot/rest/prometheus/RestPrometheusApplication.java
@@ -24,7 +24,7 @@ public class RestPrometheusApplication {
 	@ResponseBody
 	public Map<String, Object> landingPage() {
 		Counter.builder("mymetric").tag("foo", "bar").register(registry).increment();
-        return singletonMap("hello", "world2");
+        return singletonMap("hello", "world3 for preview");
 	}
 
 	public static void main(String[] args) {
